d71659b32dbb488634c8aca82e9a8258c84b0578
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2023-11-25 16:54:54 +0100

    merge the two branches

--- src/main/resources/sql/queries.sql
+++ src/main/resources/sql/queries.sql
@@ -2,7 +2,7 @@ insert into Reservations(car_id,customer_id,reservation_date,start_date,end_date
 DELETE FROM Reservations WHERE reservation_id=?;
 UPDATE Reservations SET car_id=?,customer_id=?,reservation_date=?,start_date=?,end_date=?,total_cost=?,status=? WHERE reservation_id=?;
 
-
+SELECT * from Users;
 SELECT * from Reservations;
 select * from Cars;
 select * from Customers;
